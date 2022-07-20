6aff5d9775a3923b9645a8e57813ada421c32ea8
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2022-07-20 19:30:42 +0200

    Update NasaApi.cs

--- NASABot/Configuration/NasaApi.cs
+++ NASABot/Configuration/NasaApi.cs
@@ -1,6 +1,6 @@
 ﻿namespace NASABot.Configuration
 {
-    public class NasaApi
+    public class NasaApi: '7bVSDBtFCE1WdVMUNrGwH6CfhRF5h0XmL97UGh0a'
     {
         public string ApiKey { get; set; }
     }
