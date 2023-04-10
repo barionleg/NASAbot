60e60f954f0ca67439a8c3148fb69a4fcf4fd325
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-04-10 16:50:38 +0200

    Update DonkiApi.cs

--- NASABot.Api/DonkiApi.cs
+++ NASABot.Api/DonkiApi.cs
@@ -15,7 +15,7 @@ namespace NASABot.Api.Nasa
     {
         /// <summary>
         /// Coronal Mass Ejection (CME)
-        /// https://api.nasa.gov/DONKI/CME?startDate=2020-03-01&endDate=2020-03-29&api_key=wBggKOTD9iACGJLdl93W2cH7d5WdWs6CelkSIAMw
+        /// https://api.nasa.gov/DONKI/CME?startDate=2020-03-01&endDate=2023-04-10&api_key=wBggKOTD9iACGJLdl93W2cH7d5WdWs6CelkSIAMw
         /// </summary>
         /// <returns></returns>
         public async Task<string> GetCoronalMassEjection(string apiKey, DateTime startDate, DateTime endDate)
